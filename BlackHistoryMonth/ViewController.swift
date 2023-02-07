14b45a23248a2b39304c77f1b1da48ff5f7d8f1a
SavirGordon <savirgordon83@gmail.com>
2023-02-07 13:05:30 -0500

    Initial Commit

---
+++ BlackHistoryMonth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlackHistoryMonth
+//
+//  Created by 4d on 2/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
